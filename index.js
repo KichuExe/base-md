832458caf1fdd87216fc51105317aaea9f535bd3
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-10-19 17:22:41 +0530

    added one more console!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const { connect } = require("./system/client.js"); //connecting to whatsapp web
 console.log("Connecting please wait..."); //loading..
 connect();
+console.log("Connected type .menu to see command list.")
