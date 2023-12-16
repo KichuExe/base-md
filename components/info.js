3ae7c32cdb980a3e1d2b53862788954a3dfd858b
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-12-16 15:01:56 +0530

    fix!

---
+++ components/info.js
@@ -0,0 +1,16 @@
+const {clash, runtime} = require("../lib/");
+const {tiny} = require("@toxickichux/fancytext");
+
+clash({pattern: "ping", fromMe: false, desc: "Info of bot ping.", type: "info",},
+  async ({msg}) => {
+    const start = new Date().getTime();
+    let { key } = await msg.reply(tiny("*Ping!*"));
+    const end = new Date().getTime();
+    var speed = end - start;
+    await msg.editmsg(tiny(`*Pong!*\n*Rapidity:${speed}ms*`), key);
+});
+
+clash({pattern: "runtime", fromMe: false, desc: "Info of how long the bot is alive.", type: "info",},
+  async ({msg}) => {
+  await msg.reply(tiny(`*Runtime:${await runtime()}*`));
+});
