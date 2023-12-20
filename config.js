6e643108e979f217250045dc4d63a1fae73649e0
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-12-20 17:35:59 +0530

    fix!

--- config.js
+++ config.js
@@ -1,1 +1,40 @@
-{()}
+const { Sequelize } = require("sequelize");
+const DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
+
+module.exports = {
+  LOGS: process.env.LOGS || "true", //false for if you don't want show the logs
+  HANDLERS:
+    process.env.HANDLER === "false" || process.env.HANDLER === "null"
+      ? "^[.]" // for null just "^" add this
+      : "^[.]",
+  BRANCH: "master",
+  OWNER_NUMBER: ["919961857267@s.whatsapp.net",
+"919656459062@s.whatsapp.net"],
+  STICKER_DATA: process.env.STICKER_DATA || "CLASH-WA-BOT;TOXIC-KICHUX",
+  SUDO: process.env.SUDO || "919961857267,0",
+  BASE_API: process.env.BASE_API_URL || "https://toxic-kichux-rest-api.onrender.com/api",
+  API_KEY: process.env.BASE_API_KEY || "d8da3326",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  OWNER_NAME: process.env.OWNER_NAME || "TOXIC-KICHUX",
+  BOT_NAME: process.env.BOT_NAME || "CLASH-WA-BOT",
+  WORK_TYPE: process.env.WORK_TYPE || "public",
+  DATABASE_URL: DATABASE_URL,
+  DATABASE:
+    DATABASE_URL === "./lib/database.db"
+      ? new Sequelize({
+          dialect: "sqlite",
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: "postgres",
+          ssl: true,
+          protocol: "postgres",
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
+};
