0a5970961de3b3936e557e8001a2dd2bf25099c0
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-12-21 17:17:07 +0530

    fix

--- components/converter.js
+++ components/converter.js
@@ -2,8 +2,8 @@ const { clash } = require("../lib/");
 const googleTTS = require('google-tts-api');
 
 clash({pattern: "tts", fromMe: false, desc: "A text it will convert to audio message", type: "converter",},
-async ({args , msg , conn}) => {
-if(!args) return msg.tinyreply("*_Example:.tts Hello World,en_*\n_You can check language codes from here:https://cloud.google.com/speech/docs/languages_")
+async ({args, msg, conn}) => {
+if(!args) return msg.reply("*_Example:.tts Hello World,en_*\n_You can check language codes from here:https://cloud.google.com/speech/docs/languages_")
 var text = args.split(",")[0];
 var language = args.split(",")[1];
 const audiourl = googleTTS.getAudioUrl(text, {lang: language, slow: false, host: 'https://translate.google.com',});
