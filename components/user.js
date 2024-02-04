19407d9297bed4d6bcb41df82f7eccabc388130d
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2024-02-04 16:22:30 +0530

    Delete components/user.js

--- components/user.js
+++
@@ -1,9 +0,0 @@
-const {clash, sleep} = require("../lib/");
-
-clash({pattern: "reboot", fromMe: true, desc: "Reboot the bot", type: "user",},
-async ({msg}) => {
-const { exec } = require("child_process")
-msg.tinyreply("*Rebooting...*");
-await sleep(2000)
-exec('pm2 restart all')
-});
